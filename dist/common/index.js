2561035ad83592e75532a70aa6ff2ba129939928
Galileo01 <1354541676@qq.com>
2026-01-13 11:53:13 +0800

    feat: remove useless file

--- dist/common/index.js
+++
@@ -1,5 +0,0 @@
-import path from "path";
-import { fileURLToPath } from "url";
-const common_filename = fileURLToPath(import.meta.url);
-const common_dirname = path.dirname(common_filename);
-export { common_dirname as __dirname, common_filename as __filename };
